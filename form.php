915960439f032c61aa4509157aa72ea7765c3783
sujayOrg <107000028+sujayOrg@users.noreply.github.com>
2022-11-16 03:23:04 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+$to = "emailtome.org@gmail.com";
+
+$header = "From : myportfolio.html";
+
+$txt = "Name = ".$name."/r/n Email = ".$email."/r/n Message = ".$message;
+
+mail($to,$subject,$txt,$header);
+?>
